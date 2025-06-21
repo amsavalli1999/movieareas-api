e7b23422e002bf64f772877e4056a49c3b90b10d
prasanthyoho <159232197+prasanthyoho@users.noreply.github.com>
2025-06-21 21:10:05 +0530

    Router stucture

--- main.js
+++ main.js
@@ -8,7 +8,23 @@ import express from "express";
     res.json({msg:"Hello sutdents"});
 
  }
-)
+);
+// CRUD FUNCTIONALITY
+app.get('/movies',()=>{
+
+})
+
+app.post('/movies',()=>{
+
+})
+
+app.put('/movies/:id',()=>{
+
+})
+
+app.delete('/movies/:id',()=>{
+    
+})
 
 app.listen(PORT,()=>{
     console.log(`The server is runing at http://localhost:$(PORT)`)
